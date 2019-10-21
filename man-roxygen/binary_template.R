1190eaf81d32da887a9cad840fd4ee49286903bc
Michel Lang <michellang@gmail.com>
2019-10-21 22:55:27 +0200

    cleanup

--- man-roxygen/binary_template.R
+++ man-roxygen/binary_template.R
@@ -1,10 +1,9 @@
 #' @section Meta Information:
 #' <% item = measures[[mid]] %>
-#' * Type: <%= item$type %>
-#' * Minimum value: <%= item$lower %>
-#' * Maximum value: <%= item$upper %>
-#' * Minimize: <%= item$minimize %>
-#' * Required prediction: <%= item$predict_type %>
+#' * Type: `"<%= item$type %>"`
+#' * Range: <%= format_range(item) %>
+#' * Minimize: `<%= item$minimize %>`
+#' * Required prediction: `<%= item$predict_type %>`
 #'
 #' @family Binary Classification Measures
 #' @concept binary_classification_measure
