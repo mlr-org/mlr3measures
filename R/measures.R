dcb5102f4620b389bd98e63ee638251c00a09efe
Michel Lang <michellang@gmail.com>
2021-09-23 13:29:19 +0200

    cleanup

--- R/measures.R
+++ R/measures.R
@@ -35,7 +35,7 @@ add_measure = function(obj, title, type, lower, upper, minimize) {
     type = assert_choice(type, c("binary", "classif", "regr", "similarity")),
     lower = assert_number(lower),
     upper = assert_number(upper),
-    predict_type = intersect(names(formals(obj)), c("response", "prob", "se", "sets")),
+    predict_type = intersect(names(formals(obj)), c("response", "prob", "se")),
     minimize = assert_flag(minimize, na.ok = TRUE),
     sample_weights = "sample_weights" %in% names(formals(obj))
   ), envir = measures)
