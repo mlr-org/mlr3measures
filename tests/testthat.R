7df913bc8bd2df93b221801472337932317cf0a7
Michel Lang <michellang@gmail.com>
2022-01-20 21:29:46 +0100

    chore: unify testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,5 @@
 if (requireNamespace("testthat", quietly = TRUE)) {
-  library(testthat)
-  suppressPackageStartupMessages(library(mlr3measures))
+  library("testthat")
+  suppressPackageStartupMessages(library("mlr3measures"))
   test_check("mlr3measures")
 }
