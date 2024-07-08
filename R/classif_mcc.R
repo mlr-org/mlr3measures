514eea707444bbeec41af32465c9ad694a660889
Lona <lona.koers@gmail.com>
2024-07-08 18:45:42 +0200

    multiclass mcc documentation

--- R/classif_mcc.R
+++ R/classif_mcc.R
@@ -1,24 +1,41 @@
 #' @title Matthews Correlation Coefficient
 #'
 #' @details
-#' The Matthews Correlation Coefficient is defined as \deqn{
-#'    \frac{\mathrm{TP} \cdot \mathrm{TN} - \mathrm{FP} \cdot \mathrm{FN}}{\sqrt{(\mathrm{TP} + \mathrm{FP}) (\mathrm{TP} + \mathrm{FN}) (\mathrm{TN} + \mathrm{FP}) (\mathrm{TN} + \mathrm{FN})}}.
+#' In the binary case, the Matthews Correlation Coefficient is defined as \deqn{
+#'    \frac{\mathrm{TP} \cdot \mathrm{TN} - \mathrm{FP} \cdot \mathrm{FN}}{\sqrt{(\mathrm{TP} + \mathrm{FP}) (\mathrm{TP} + \mathrm{FN}) (\mathrm{TN} + \mathrm{FP}) (\mathrm{TN} + \mathrm{FN})}},
 #' }{
-#'    (TP * TN - FP * FN) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN)).
+#'    (TP * TN - FP * FN) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN)),
 #' }
+#' where \eqn{TP}, \eqn{FP}, \eqn{TN}, \eqn{TP} are the number of true positives, false positives, true negatives, and false negatives respectively.
+#'
+#' In the multi-class case, the Matthews Correlation Coefficient defined for a multi-class confusion matrix \enq{C} with \eqn{K} classes: \deqn{
+#'    \frac{c \cdot s - \sum_k^K p_k \cdot t_k}{\sqrt{(s^2 - \sum_k^K p_k^2) \cdot (s^2 - \sum_k^K t_k^2)}},
+#' }{
+#'    (c * s - sum(pk * tk)) / sqrt((s^2 - sum(pk^2)) * (s^2 - sum(tk^2))),
+#' }
+#' where
+#' - \eqn{s = \sum_i^K \sum_j^K C_{ij}}: total number of samples
+#' - \eqn{c = \sum_k^K C_{kk}}: total number of correctly predicted samples
+#' - \eqn{t_k = \sum_i^K C_{ik}}: number of predictions for each class \eqn{k}
+#' - \eqn{p_k = \sum_j^K C_{kj}}: number of true occurrences for each class \eqn{k}.
 #'
 #' @templateVar mid mcc
-#' @template binary_template
+#' @template classif_template
 #'
 #' @details
 #' This above formula is undefined if any of the four sums in the denominator is 0.
 #' The denominator is then set to 1.
+#' For the multi-class version, the minimum range is between -1 and 0 depending on the data.
 #'
 #' @references
+#' \url{https://en.wikipedia.org/wiki/Phi_coefficient}
+#'
 #' `r format_bib("matthews_1975")`
 #'
-#' @inheritParams binary_params
-#' @template binary_example
+#' @inheritParams classif_params
+#' @param positive (`character(1`) Name of the positive class in case of binary classification.
+#'
+#' @template classif_example
 #' @export
 mcc = function(truth, response, positive = NULL, ...) {
   assert_classif(truth, response = response)
