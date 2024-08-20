343809802a2d3ac0713430adc99bccd6460a4cc1
be-marc <marcbecker@posteo.de>
2024-08-20 17:02:16 +0200

    docs: update

--- R/regr_pinball.R
+++ R/regr_pinball.R
@@ -1,12 +1,24 @@
-#' @title Pinball
+#' @title Average Pinball Loss
 #'
 #' @details
-#' The pinball loss for quantile regression.
+#' The pinball loss for quantile regression is defined as \deqn{
+#' \text{Average Pinball Loss} = \frac{1}{n} \sum_{i=1}^{n} w_{i}
+#' \begin{cases}
+#' q \cdot (t_i - r_i) & \text{if } t_i \geq r_i \\
+#'(1 - q) \cdot (r_i - t_i) & \text{if } t_i < r_i
+#' \end{cases}
+#' }
+#' where \eqn{q} is the quantile and \eqn{w_i} are normalized sample weights.
+#'
 #'
 #' @templateVar mid pinball
 #' @template regr_template
 #'
 #' @inheritParams regr_params
+#'
+#' @param alpha `numeric(1)`\cr
+#' Alpha level.
+#'
 #' @template regr_example
 #' @export
 pinball = function(truth, response, sample_weights = NULL, alpha = 0.5, ...) {
